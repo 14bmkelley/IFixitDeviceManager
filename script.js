9da328f1b8c04020a512bf4db6e1e0461d0942b7
Brandon Kelley <14bmkelley@gmail.com>
2016-10-25 10:46:21 -0700

    Successful API hits

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 function load() {
 
-   var recentSearch = false;
-
-   $$(".search").addEvent("click", function() {
-      
-      if (recentSearch) return;
-
-
-
-   });
+   setupSearch();
 
 };
+
+function setupSearch() {
+   
+   // On key up event, make a search request and display results
+   $$(".search-input").addEvent("keyup", function(event) {
+
+      var searchText = $$(".search-input").get("value");
+      if (searchText === "") return;
+
+      var searchQuery = "https://www.ifixit.com/api/2.0/suggest/"
+         + encodeURIComponent(searchText);
+
+      $$(".search-results").set("display", "none");
+
+      var searchRequest = new Request({
+         "url": searchQuery,
+         "method": "get",
+         "link": "cancel",
+         "onSuccess": function(responseText) {
+            console.log(responseText);
+         }
+      });
+
+      searchRequest.send();
+   
+   });
+
+}
